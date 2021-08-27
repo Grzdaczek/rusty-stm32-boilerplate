23c24fce1969119e2747684faf92dcdd3b644c44
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-08-27 16:13:33 +0200

    blinky

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use cortex_m_rt::{entry, exception};
+use stm32f1::stm32f103;
+use stm32f1::stm32f103::Peripherals;
+
+static mut SYSTICK_QUEUED: bool = false;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = stm32f103::Peripherals::take().unwrap();
+    config_gpio(&peripherals);
+    config_stk(& peripherals);
+
+    let gpiob = &peripherals.GPIOB;
+    let mut led_state = false;
+
+    loop {
+        unsafe { 
+            while !SYSTICK_QUEUED {} 
+            SYSTICK_QUEUED = false
+        }
+        led_state = !led_state;
+        gpiob.odr.write(|w| match led_state {
+            true => w.odr12().set_bit(),
+            false => w.odr12().clear_bit(),
+        });
+    }
+}
+
+fn config_gpio(p: &Peripherals) {
+    p.RCC.apb2enr.modify(|_, w| w.iopben().set_bit()); // enable clock for GPIOB
+    p.GPIOB.crh.modify(|_, w| {
+        w
+            .mode12().bits(0b01) // 10MHz output
+            .cnf12().bits(0b00) // push-pull ouptut
+    });
+}
+
+fn config_stk(p: & Peripherals) {
+    p.STK.ctrl.modify(|_, w| {
+        w
+            .enable().set_bit()
+            .tickint().set_bit()
+            .clksource().set_bit()
+            .countflag().set_bit()
+    });
+    p.STK.load_.modify(|_, w| unsafe {
+        w.reload().bits(1_000_000)
+    });
+}
+
+#[exception]
+fn SysTick() {
+    unsafe { SYSTICK_QUEUED = true };
 }
