a7c0eac79c80e118fcf7654d4b5014c260b0d802
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-09-01 23:13:18 +0200

    UART config [WIP]

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,8 @@ fn main() -> ! {
     // hprintln!("-------->> HELLO RUST! <<--------").unwrap();
     let peripherals = stm32f103::Peripherals::take().unwrap();
     config_gpio(&peripherals);
-    config_stk(& peripherals);
+    config_stk(&peripherals);
+    config_uart(&peripherals);
 
     let gpioa = &peripherals.GPIOA;
     let mut led_state = false;
@@ -31,7 +32,7 @@ fn main() -> ! {
         gpioa.odr.write(|w| match led_state {
             true => w.odr5().set_bit(),
             false => w.odr5().clear_bit(),
-        });
+        }); 
     }
 }
 
@@ -39,24 +40,48 @@ fn main() -> ! {
 
 fn config_gpio(p: &Peripherals) {
     p.RCC.apb2enr.modify(|_, w| w.iopaen().set_bit()); // enable clock for GPIOA
-    p.GPIOA.crl.modify(|_, w| {
-        w
-            .mode5().bits(0b01) // 10MHz output
-            .cnf5().bits(0b00) // push-pull ouptut
-    });
+    p.GPIOA.crl.modify(|_, w| w
+        // LED
+        .mode5().bits(0b01) // 10MHz
+        .cnf5().bits(0b00) // push-pull ouptut
+    );
 }
 
-fn config_stk(p: & Peripherals) {
-    p.STK.ctrl.modify(|_, w| {
-        w
-            .enable().set_bit()
-            .tickint().set_bit()
-            .clksource().set_bit()
-            .countflag().set_bit()
-    });
+fn config_stk(p: &Peripherals) {
+    p.STK.ctrl.modify(|_, w| w
+        .enable().set_bit()
+        .tickint().set_bit()
+        .clksource().set_bit()
+        .countflag().set_bit()
+    );
     p.STK.load_.modify(|_, w| unsafe {
         w.reload().bits(1_000_000)
     });
+}
+
+fn config_uart(p: &Peripherals) {
+    p.RCC.apb1enr.modify(|_, w| w.usart2en().set_bit()); // enable clock for USART2
+    p.GPIOA.crl.modify(|_, w| w
+        // USART2 TX
+        .mode2().bits(0b01) // 10MHz
+        .cnf2().bits(0b10) // alternae push-pull
+
+        // USART2 RX
+        .mode3().bits(0b00) // Reserved
+        .cnf3().bits(0b10) // Input floating
+    );
+    p.GPIOA.odr.modify(|_, w| w
+        // USART RX
+        .odr3().set_bit() // Input pull-up
+    );
+    p.USART2.brr.modify(|_, w| w
+        .div_fraction().bits(0)
+        .div_mantissa().bits(0)
+    );
+    p.USART2.cr1.modify(|_, w| w
+        .ue().set_bit()
+    );
+    // p.USART2.cr2.modify(f)
 }
 
 #[exception]
